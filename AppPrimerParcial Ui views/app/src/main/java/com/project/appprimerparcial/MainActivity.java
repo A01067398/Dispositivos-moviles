51c90beb934b5ae37f39b64417687aa67783e55d
A01067398 <yadagme27@gmail.com>
2022-03-29 20:54:41 -0600

    Ui views, ejercicio de UI views, mejorando la app anterior

---
+++ AppPrimerParcial Ui views/app/src/main/java/com/project/appprimerparcial/MainActivity.java
@@ -0,0 +1,46 @@
+package com.project.appprimerparcial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnTopic3, btnTopic4, btnClose;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnTopic3 = findViewById(R.id.btnTopic3);
+        btnTopic3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent frmUsuario = new Intent(getApplicationContext(), FrmUsuarioActivity.class);
+                startActivity(frmUsuario);
+            }
+        });
+
+        btnTopic4 = findViewById(R.id.btnTopic4);
+        btnTopic4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent frmRateStars = new Intent(getApplicationContext(), RateStarsActivity.class);
+                startActivity(frmRateStars);
+            }
+        });
+
+        btnClose = findViewById(R.id.btnClose);
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
+
+    }
+}
